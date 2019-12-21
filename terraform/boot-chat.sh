8b85a2c988ac28ebbd7b02b7b479f7cbcdc3fe5a
Jacob Parker <jacob@solidangle.ca>
2019-12-21 14:22:29 -0500

    Get the deploy key from the keys bucket
    Baking it into the startup script was risking it being displayed in the
    terraform plan. This is more consistent anyway.

--- terraform/boot-chat.sh
+++ terraform/boot-chat.sh
@@ -2,12 +2,34 @@
 
 set -euo pipefail
 
-echo "Creating the synapse user"
-id -u synapse &>/dev/null || useradd -u 1337 synapse
+if [ ! -f GCSFUSE_IS_INSTALLED ]; then
+  echo "Installing gcsfuse"
+  GCSFUSE_REPO=gcsfuse-$(lsb_release -c -s)
+  echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+  apt-get update
+  apt-get install gcsfuse
+  touch GCSFUSE_IS_INSTALLED
+fi
+
+if [ ! id -u synapse > /dev/null 2>&1 ]; then
+  echo "Creating the synapse user"
+  useradd -u 1337 synapse
+  usermod -a -G fuse synapse
+fi
+
+echo "Creating the mount point for keys"
+mkdir keys
+chown synapse:synapse keys
 
 echo "Dropping permissions"
 su - synapse
 
+echo "Mounting the keys bucket"
+gcsfuse cmc-chat-keys keys
+
+export GIT_SSH_COMMAND='ssh -i ~/keys/github/deploy_key'
 if [ -d "cmc" ]; then
   echo "Repo directory exists; pulling"
   cd cmc
