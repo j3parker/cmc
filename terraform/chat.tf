136bafa9564ae7c5bdda89cefcf995c0f64b59c3
Jacob Parker <jacob@solidangle.ca>
2019-12-21 18:05:52 -0500

    Open up HTTP/HTTPS on the firewall (#30)

--- terraform/chat.tf
+++ terraform/chat.tf
@@ -19,6 +19,7 @@ resource "google_compute_instance" "chat" {
   name         = "chat"
   machine_type = "n1-standard-1"
   zone         = "${local.zone}"
+  tags         = ["web"]
 
   boot_disk {
     initialize_params {
@@ -39,4 +40,26 @@ resource "google_compute_instance" "chat" {
 
   metadata_startup_script   = file("boot-chat.sh")
   allow_stopping_for_update = true
+}
+
+resource "google_compute_firewall" "chat" {
+  name    = "chat"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web"]
 }
