98fb25b5ce012a0f8b3196b6d7604d073a3a0409
Jacob Parker <jacob@solidangle.ca>
2019-12-21 22:32:14 -0500

    Useless cat incurs the wrath of pipefail (#43)

--- chat/synapse/init.sh
+++ chat/synapse/init.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 echo "Generating secret"
-REG_SECRET=$(cat /dev/urandom | head -c63 | base64)
+REG_SECRET=$(head -c63 /dev/urandom | base64)
 echo "The shared registration secret for this boot is $REG_SECRET ... keep it a secret! :)"
 echo "registration_shared_secret: $REG_SECRET" >> ~/homeserver.yaml
