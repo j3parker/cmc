a3027af419fd2d3dbee62ddddd4ce9013446360b
Jacob Parker <jacob@solidangle.ca>
2019-12-21 22:14:15 -0500

    Split init and command (#42)

--- chat/synapse/init.sh
+++ chat/synapse/init.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail
 
+echo "Generating secret"
 REG_SECRET=$(cat /dev/urandom | head -c63 | base64)
-
 echo "The shared registration secret for this boot is $REG_SECRET ... keep it a secret! :)"
-
-echo "registration_shared_secret: $REG_SECRET" >> ~/homeserver.yml
-
-python3 -B -m synapse.app.homeserver -c homeserver.yaml
+echo "registration_shared_secret: $REG_SECRET" >> ~/homeserver.yaml
